516a38f224bd3504d7162100f3f2a325ab8c3d37
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-27 18:50:55 +0600

    navbar sign out in functionalized.

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import "./NavigationBar.css";
 import { Link } from 'react-router-dom';
-import { useSignOut } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const NavigationBar = () => {
 
-    const [signOut, loading, error] = useSignOut(auth);
+
+    const [user, loading, error] = useAuthState(auth);
+    const [signOut, singOutLoading, singOutError] = useSignOut(auth);
 
     return (
         <nav>
             navigation bar
-            <button onClick={() => signOut()}>sign out</button>
-            <Link to="/signin">sign in</Link>
+
+            {
+                user ? <button onClick={() => signOut()}>sign out</button> : <Link to="/signin">sign in</Link>
+            }
+
         </nav>
     );
 };
