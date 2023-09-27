7b739910d33caf43e83446628932374a5c76e58f
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-27 22:54:30 +0600

    require loading addded.

--- src/Components/RequireAuth/RequireAuth.js
+++ src/Components/RequireAuth/RequireAuth.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useNavigate } from 'react-router-dom';
+import Loading from '../SignIn/Loading';
 
 const RequireAuth = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
+
+    if (loading) {
+        <Loading></Loading>
+    }
 
     if (!user) {
         navigate("/signin");
