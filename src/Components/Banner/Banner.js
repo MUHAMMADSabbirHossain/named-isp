3bf1836792756f3a4e5a5bb7ad6f77d544d63c24
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-27 12:43:33 +0600

    components added.

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Banner";
+
+const Banner = () => {
+    return (
+        <div>
+            banner
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NotFound.css";
+
+const NotFound = () => {
+    return (
+        <div>
+            not found
+        </div>
+    );
+};
+
+export default NotFound;
