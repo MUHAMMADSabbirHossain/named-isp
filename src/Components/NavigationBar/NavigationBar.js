b940a089dfc841649f87565784f654319272817c
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-27 18:46:51 +0600

    profile name added.

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -5,11 +5,13 @@ import { useSignOut } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const NavigationBar = () => {
+
     const [signOut, loading, error] = useSignOut(auth);
+
     return (
         <nav>
             navigation bar
-            <button onClick={signOut}>sign out</button>
+            <button onClick={() => signOut()}>sign out</button>
             <Link to="/signin">sign in</Link>
         </nav>
     );
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./Profile.css";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Profile = () => {
+
+    const [user, loading, error] = useAuthState(auth);
     return (
         <div>
             profile
+            {
+                user && <h2>{user.email}</h2>
+
+            }
         </div>
     );
 };
